093aee7474673a8f6a34e0f485eb863c32ab2861
Vincent Nguyen <vincen7.nguyen@gmail.com>
2025-04-03 10:34:04 +0200

    Add helper scripts

---
+++ create_env.ps1
@@ -0,0 +1,4 @@
+Set-ExecutionPolicy RemoteSigned -Scope Process
+python -m venv env
+python -m pip install -r requirements.txt
+.\env\Scripts\Activate.ps1
---
+++ edybot_launch.ps1
@@ -0,0 +1,3 @@
+cd "C:\User\edi-thymio\Downloads\EdyBot-main\EdyBot-main\edybot_launch.ps1"
+.\env\Scripts\Activate.ps1
+python -m chat_min
