f5b8d4fbd3d02d77b11b81949ae6cd04b52e4cc4
Vincent Nguyen <vincen7.nguyen@gmail.com>
2025-04-08 07:00:58 +0200

    Fix path for launch file

--- edybot_launch.ps1
+++ edybot_launch.ps1
@@ -1,3 +1,3 @@
-cd "C:\User\edi-thymio\Downloads\EdyBot-main\EdyBot-main\edybot_launch.ps1"
+cd "C:\User\edi-thymio\Downloads\EdyBot-main\EdyBot-main"
 .\env\Scripts\Activate.ps1
 python -m chat_min
